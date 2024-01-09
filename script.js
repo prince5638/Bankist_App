013adc3fbac4a76fd729990177f3e38688feaa9f
Prince Changani <princechangani56385638@gmail.com>
2024-01-09 18:22:57 +0530

    Adding movements of the users using DOM
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- script.js
+++ script.js
@@ -61,6 +61,30 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+
+  // emptying dummy movements
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function(mov, i){
+
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">
+          ${i + 1}${type}
+        </div>
+        <div class="movements__date">0 days ago</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
